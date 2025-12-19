887bd91c831dca58baf030c067561dcc0410e328
Sebastian Ramacher <Sebastian.Ramacher@ait.ac.at>
2025-12-19 15:17:26 +0100

    Upgrade to criterion 0.8

--- benches/engine.rs
+++ benches/engine.rs
@@ -1,5 +1,7 @@
+use std::hint::black_box;
+
 use bls12_381_relic::{G1Projective, G2Projective, RelicEngine, ff::Field};
-use criterion::{Criterion, black_box, criterion_group, criterion_main};
+use criterion::{Criterion, criterion_group, criterion_main};
 use pairing::{
     Engine, MillerLoopResult, MultiMillerLoop,
     group::{Curve, Group, prime::PrimeCurve},
